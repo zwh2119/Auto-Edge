4a0b79b9c29f81e42ffbb45f14085c32d5110a69
onecheck <1548767361@qq.com>
2023-12-14 11:29:20 +0800

    update AutoEdge v0.7.0

--- test/car_detection_trt_test.py
+++ test/car_detection_trt_test.py
@@ -1,6 +1,8 @@
 import json
 import os
+import shutil
 import time
+import  random
 
 import cv2
 import requests
@@ -24,6 +26,11 @@ def resolution2text(resolution: tuple):
 
 
 def generate():
+    write_dir = 'output'
+    if os.path.exists(write_dir):
+        shutil.rmtree(write_dir)
+    os.mkdir(write_dir)
+
     cur_id = 0
     cnt = 0
 
@@ -58,7 +65,7 @@ def generate():
         fps_raw = cap.get(cv2.CAP_PROP_FPS)
 
         # adjust resolution
-        frame = cv2.resize(frame, (640,640))
+        # frame = cv2.resize(frame, (640,640))
 
         # adjust fps
         cnt += 1
@@ -76,20 +83,61 @@ def generate():
             # compress frames in the buffer into a short video
             compressed_video_pth = compress_frames(temp_frame_buffer, frame_fourcc)
 
-            # start record transmit time
-
-            # post task to local controller
-            requests.post(service_address,
+            response = requests.post(service_address,
                           data={'data': json.dumps(None)},
                           files={'file': (f'tmp.mp4',
                                           open(compressed_video_pth, 'rb'),
                                           'video/mp4')})
+            data = response.json()['result']
+            for i in range(len(temp_frame_buffer)):
+                boxes = data[i]
+                frame = temp_frame_buffer[i]
+                for box in boxes:
+                    plot_one_box(box, frame, label='')
+                cv2.imwrite(os.path.join(write_dir, f'output_{i}.png'), frame)
 
             cur_id += 1
             temp_frame_buffer = []
             os.remove(compressed_video_pth)
 
             time.sleep(2)
+            break
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+    """
+    description: Plots one bounding box on image img,
+                 this function comes from YoLov5 project.
+    param:
+        x:      a box likes [x1,y1,x2,y2]
+        img:    a opencv image object
+        color:  color to draw rectangle, such as (0,255,0)
+        label:  str
+        line_thickness: int
+    return:
+        no return
+
+    """
+    tl = (
+        line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1
+    )  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(
+            img,
+            label,
+            (c1[0], c1[1] - 2),
+            0,
+            tl / 3,
+            [225, 255, 255],
+            thickness=tf,
+            lineType=cv2.LINE_AA,
+        )
 
 
 def get_fps_adjust_mode(fps_raw, fps):
