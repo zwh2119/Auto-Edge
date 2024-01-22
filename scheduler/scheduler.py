ac1b19ed3d7b622a67f514e404dd12f215ea133c
onecheck <1548767361@qq.com>
2024-01-22 22:42:36 +0800

    update scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -127,19 +127,19 @@ class Scheduler:
         done = False
         if pid_out > 0:
 
-            if pid_out > 1:
+            if pid_out > 0.1:
                 fps, done = self.change_single_configuration(self.fps_list, 1, fps, fps_raw)
-            if pid_out > 2 or not done:
+            if pid_out > 0.2 or not done:
                 resolution, done = self.change_single_configuration(self.resolution_list, 1, resolution, resolution_raw)
-            if pid_out > 3 or not done:
+            if pid_out > 0.3 or not done:
                 position, done = self.change_position(position, source_device, 1)
         if pid_out < 0:
-            if pid_out < -3 or not done:
+            if pid_out < -0.3 or not done:
                 position, done = self.change_position(position, source_device, -1)
-            if pid_out < -2 or not done:
+            if pid_out < -0.2 or not done:
                 resolution, done = self.change_single_configuration(self.resolution_list, -1, resolution,
                                                                     resolution_raw)
-            if pid_out < -1:
+            if pid_out < -0.1:
                 fps, done = self.change_single_configuration(self.fps_list, -1, fps, fps_raw)
 
         return {
