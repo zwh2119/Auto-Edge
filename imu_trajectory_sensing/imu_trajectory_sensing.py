be9e10c2a639146ca02b985fab907b289e57b09d
onecheck <1548767361@qq.com>
2024-01-24 14:52:34 +0800

    update AutoEdge mid-v0.2.3

--- imu_trajectory_sensing/imu_trajectory_sensing.py
+++ imu_trajectory_sensing/imu_trajectory_sensing.py
@@ -9,67 +9,10 @@ from scipy.integrate import cumtrapz
 
 
 class ImuProcessor1:
-    def __init__(self, id: str, incoming_mq_topic: str, outgoing_mq_topic: str,
-                 priority: int, tuned_parameters: dict,
-                 mqtt_host: str = 'localhost', mqtt_port: int = 1883, mqtt_username: str = 'admin',
-                 mqtt_password: str = 'admin'):
-        super().__init__(id, incoming_mq_topic, outgoing_mq_topic, priority, tuned_parameters)
-        mqtt_client_id = str(id)
-        self.subscriber = MqttSubscriber(mqtt_host, mqtt_port, mqtt_username, mqtt_password,
-                                         mqtt_client_id + "_subscriber")
-        self.publisher = MqttPublisher(mqtt_host, mqtt_port, mqtt_username, mqtt_password,
-                                       mqtt_client_id + "_publisher")
-        # This will be accessed by different threads, so we need to use a lock
-        self.lock = threading.Lock()
-        self.local_task_queue = []
-
-    @classmethod
-    def processor_type(cls) -> str:
-        return 'imu'
-
-    @classmethod
-    def processor_description(cls) -> str:
-        return 'Imu processor1'
-
-    def get_id(self) -> str:
-        return self._id
-
-    def get_incoming_mq_topic(self) -> str:
-        return self._incoming_mq_topic
-
-    def get_outgoing_mq_topic(self) -> str:
-        return self._outgoing_mq_topic
-
-    def get_priority(self) -> int:
-        return self._priority
-
-    def set_priority(self, priority: int):
-        self._priority = priority
-
-    def get_tuned_parameters(self) -> dict:
-        return self._tuned_parameters
-
-    def set_tuned_parameters(self, tuned_parameters: dict):
-        self._tuned_parameters = tuned_parameters
-
-    def get_task_from_incoming_mq(self) -> ImuTask:
-        with self.lock:
-            return self.local_task_queue.pop(0)
-
-    def send_task_to_outgoing_mq(self, task: ImuTask):
-        self.publisher.publish(self._outgoing_mq_topic, json.dumps(task.serialize()), qos=2)
+    def __init__(self):
+        pass
 
     def run(self):
-        self.subscriber.subscribe(self._incoming_mq_topic,
-                                  callback=(lambda client, userdata, message: (
-                                      self.lock.acquire(),
-                                      self.local_task_queue.append(
-                                          ImuTask.deserialize(json.loads(message.payload.decode()))),
-                                      self.lock.release())),
-                                  qos=2
-                                  )
-        self.subscriber.client.loop_start()
-        self.publisher.client.loop_start()
         while True:
             if len(self.local_task_queue) > 0:
                 # task = self.get_task_from_incoming_mq()
@@ -89,9 +32,8 @@ class ImuProcessor1:
                 # data = base64.b64decode(task.get_data().encode('utf-8'))
                 data = np.frombuffer(base64.b64decode(task.get_data().encode('utf-8'))).reshape((-1, 7))
                 process_result = self.getPCA4(data, R)
-                # print(f"Processed result: {process_result}")
-                processed_task = ImuTask(process_result, task.get_seq_id(), task.get_source_id(), self.get_priority())
-                self.send_task_to_outgoing_mq(processed_task)
+
+
 
     def getPCA4(self, data, R):
         lpms_time = data[:, 0]
