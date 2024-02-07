68c0e736aa9ad96b57ed065466189624d388891a
onecheck <1548767361@qq.com>
2024-02-07 22:46:26 +0800

    rebuild code structure

--- core/lib/common/video_ops.py
+++ core/lib/common/video_ops.py
@@ -1,19 +1,20 @@
+from core.lib.common import reverse_key_value_in_dict
+
+
 class VideoOps:
+    resolution_dict = {'1080p': (1920, 1080),
+                       '720p': (1280, 720),
+                       '480p': (640, 480),
+                       '360p': (640, 360)}
 
-    # TODO: here config file?
-    @staticmethod
-    def text2resolution(text: str):
-        resolution_dict = {'1080p': (1920, 1080),
-                           '720p': (1280, 720),
-                           '360p': (640, 360)}
+    resolution_dict_reverse = reverse_key_value_in_dict(resolution_dict)
 
-        assert text in resolution_dict
-        return resolution_dict[text]
+    @classmethod
+    def text2resolution(cls, text: str):
+        assert text in cls.resolution_dict, f'Invalid resolution "{text}"!'
+        return cls.resolution_dict[text]
 
-    @staticmethod
-    def resolution2text(resolution: tuple):
-        resolution_dict = {(1920, 1080): '1080p',
-                           (1280, 720): '720p',
-                           (640, 360): '360p'}
-        assert resolution in resolution_dict
-        return resolution_dict[resolution]
+    @classmethod
+    def resolution2text(cls, resolution: tuple):
+        assert resolution in cls.resolution_dict_reverse, f'Invalid resolution "{resolution}"!'
+        return cls.resolution_dict_reverse[resolution]
