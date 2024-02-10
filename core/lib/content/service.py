5d7a9c4ff325637dc28dd9b0011d8ee81f9f672a
onecheck <1548767361@qq.com>
2024-02-10 22:55:48 +0800

    rebuild code structure

--- core/lib/content/service.py
+++ core/lib/content/service.py
@@ -1,14 +1,46 @@
 class Service:
-    def __init__(self):
-        self.service_name = ''
-        self.transmit_time = 0
-        self.service_time = 0
+    def __init__(self, service_name, execute_device=''):
+        self.__service_name = service_name
+        self.__execute_device = execute_device
+        self.__transmit_time = 0
+        self.__execute_time = 0
 
+    def get_service_name(self, service_name):
+        self.__service_name = service_name
+
+    def get_execute_device(self):
+        return self.__execute_device
+
+    def set_execute_device(self, execute_device):
+        self.__execute_device = execute_device
+
+    def set_transmit_time(self, transmit_time):
+        assert transmit_time >= 0, f'transmit time of service "{self.__service_name}" is negative: {transmit_time}!'
+        self.__transmit_time = transmit_time
+
+    def set_execute_time(self, execute_time):
+        assert execute_time >= 0, f'execute time of service "{self.__service_name}" is negative: {execute_time}!'
+        self.__execute_time = execute_time
+
+    def get_transmit_time(self):
+        return self.__transmit_time
+
+    def get_execute_time(self):
+        return self.__execute_time
+
+    def get_service_total_time(self):
+        return self.__execute_time + self.__execute_time
 
     @staticmethod
     def serialize(service):
-        pass
+        return {'service_name': service.__service_name,
+                'execute_device': service.__execute_device,
+                'execute_data': {'transmit_time': service.__transmit_time, 'execute_time': service.__execute_time}
+                }
 
     @staticmethod
-    def deserialize(service):
-        pass
+    def deserialize(data):
+        service = Service(data['service_name'], data['execute_device'])
+        service.set_execute_time(data['execute_data']['execute_time'])
+        service.set_transmit_time(data['execute_data']['transmit_time'])
+
