fb2c87dab5ac57f274f41fd80d2c57980bf0a93e
onecheck <1548767361@qq.com>
2024-02-11 17:50:40 +0800

    rebuild code structure

--- core/lib/estimation/time_estimation.py
+++ core/lib/estimation/time_estimation.py
@@ -4,9 +4,9 @@ import time
 class TimeEstimator:
 
     @staticmethod
-    def record_pipeline_time(data: dict, flow_index: int, transmit: bool = True) -> (bool, dict, float):
+    def record_pipeline_ts(data: dict, flow_index: int, transmit: bool = True) -> (bool, dict, float):
         """
-        record time in system
+        record pipeline timestamp in system
         :param data: time dictionary
         :param flow_index: index in pipeline flow
         :param transmit: is transmit time record (transmit time or service time)
@@ -15,12 +15,12 @@ class TimeEstimator:
                  duration: time estimation result
 
         """
-        return TimeEstimator.record_time(data, f'transmit_time_{flow_index}' if transmit else f'service_time_{flow_index}')
+        return TimeEstimator.record_ts(data, f'transmit_time_{flow_index}' if transmit else f'service_time_{flow_index}')
 
     @staticmethod
-    def record_time(data: dict, tag: str) -> (bool, dict, float):
+    def record_ts(data: dict, tag: str) -> (bool, dict, float):
         """
-        record time in system
+        record timestamp in system
         :param data: time dictionary
         :param tag: name of time ticket
         :return: is_end: is the end of time estimation
@@ -41,3 +41,26 @@ class TimeEstimator:
             duration = 0
 
         return is_end, data, duration
+
+    @staticmethod
+    def estimate_duration_time(func):
+        def wrapper(*args, **kw):
+            start = time.time()
+            func(*args, **kw)
+            end = time.time()
+
+            return end-start
+
+        return wrapper
+
+    @staticmethod
+    def estimate_duration_time_print(func):
+        def wrapper(*args, **kw):
+            start = time.time()
+            func(*args, **kw)
+            end = time.time()
+
+            print('function {} cost time {:.2f}s'.format(func.__name__, end-start))
+
+        return wrapper
+
