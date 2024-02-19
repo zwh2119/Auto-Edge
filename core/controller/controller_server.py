91daa99ed5013a27d696709b74c8fde1485213e3
onecheck <1548767361@qq.com>
2024-02-19 16:55:17 +0800

    rebuild code structure

--- core/controller/controller_server.py
+++ core/controller/controller_server.py
@@ -1,3 +1,25 @@
+from fastapi import FastAPI, BackgroundTasks, UploadFile, File, Form
+
+from fastapi.routing import APIRoute
+from starlette.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from core.lib.network.api import NetworkAPIPath, NetworkAPIMethod
+
+
 class ControllerServer:
     def __init__(self):
+        self.app = FastAPI(routes=[
+            APIRoute(NetworkAPIPath.CONTROLLER_TASK,
+                     self.submit_task,
+                     response_class=JSONResponse,
+                     methods=[NetworkAPIMethod.CONTROLLER_TASK]
+                     ),
+        ], log_level='trace', timeout=6000)
+
+        self.app.add_middleware(
+            CORSMiddleware, allow_origins=["*"], allow_credentials=True,
+            allow_methods=["*"], allow_headers=["*"],
+        )
+
+    def submit_task(self):
         pass
