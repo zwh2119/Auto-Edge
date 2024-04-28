4261d6200811cc098cb08d79cee769e18a2a8e6b
onecheck <1548767361@qq.com>
2024-04-29 00:47:08 +0800

    rebuild code structure

--- tools/build.sh
+++ tools/build.sh
@@ -126,11 +126,12 @@ build_image_special() {
     echo "Building image: $temp_tag on platform: $platform using Dockerfile: $dockerfile with no-cache: $NO_CACHE"
 
     if [ -z "$cache_option" ]; then
-        docker   buildx build --platform="$platform"  -t "$temp_tag" -f "$dockerfile" "$context_dir" --output=type=registry --push
+        DOCKER_CLI_EXPERIMENTAL=enabled docker  build --platform="$platform"  -t "$temp_tag" -f "$dockerfile" "$context_dir"
     else
-        docker   buildx build  --platform="$platform"  -t "$temp_tag" -f "$dockerfile" "$context_dir" "$cache_option" --output=type=registry --push
-
+        DOCKER_CLI_EXPERIMENTAL=enabled docker  build  --platform="$platform"  -t "$temp_tag" -f "$dockerfile" "$context_dir" "$cache_option"
     fi
+
+    docker push "$temp_tag"
 }
 
 create_and_push_manifest() {
