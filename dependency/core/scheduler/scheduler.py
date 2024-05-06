ba7addcaa8dadd2c3703d6a5f6787c948ff03d7c
onecheck <1548767361@qq.com>
2024-05-06 22:25:35 +0800

    rebuild core structure

--- dependency/core/scheduler/scheduler.py
+++ dependency/core/scheduler/scheduler.py
@@ -1,12 +1,32 @@
+import time
+
+from core.lib.common import Context, FileNameConstant, YamlOps
+
+
 class Scheduler:
     def __init__(self):
-        pass
+        self.schedule_table = {}
+        self.resource_table = {}
+
+        config_file_path = Context.get_file_path(FileNameConstant.SCHEDULE_CONFIG.value)
+        configs = YamlOps.read_yaml(config_file_path)
+        self.fps_list = configs['fps']
+        self.resolution_list = configs['resolution']
+
+        self.schedule_interval = 1
 
     def register_schedule_table(self, source_id):
-        pass
+        if source_id in self.schedule_table:
+            return
 
     def get_schedule_plan(self, info):
-        pass
+        return {
+            'resolution': '720p',
+            'fps': 15,
+            'encoding': 'mp4v',
+            'batch_size': 8,
+            'pipeline': info['pipeline']
+        }
 
     def update_scheduler_scenario(self, info):
         pass
@@ -19,7 +39,3 @@ class Scheduler:
 
     def run(self):
         pass
-
-
-
-
