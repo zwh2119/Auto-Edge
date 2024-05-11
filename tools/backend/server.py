2b9126ab7f0cbafa52b170513a60281165e4465f
onecheck <1548767361@qq.com>
2024-05-11 16:21:41 +0800

    rebuild code structure

--- tools/backend/server.py
+++ tools/backend/server.py
@@ -12,7 +12,9 @@ import uvicorn
 from fastapi import FastAPI, Body, Request, File, UploadFile
 from starlette.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-import requests
+
+import signal
+import functools
 
 from kube_helper import KubeHelper
 import yaml
@@ -25,8 +27,35 @@ import sys
 sys.path.append('/home/hx/zwh/Auto-Edge-rebuild/dependency')
 
 from core.lib.content import Task
-from core.lib.common import timeout, LOGGER
+from core.lib.common import LOGGER
 from core.lib.network import http_request
+
+
+def timeout(sec):
+    """
+    timeout decorator
+    :param sec: function raise TimeoutError after ? seconds
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped_func(*args, **kwargs):
+
+            def _handle_timeout(signum, frame):
+                err_msg = f'Function {func.__name__} timed out after {sec} seconds'
+                raise TimeoutError(err_msg)
+
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(sec)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wrapped_func
+
+    return decorator
 
 
 class NameCounter:
