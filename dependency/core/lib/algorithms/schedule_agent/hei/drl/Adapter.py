b6cb8c8a11c9d42296e99f914ed160f19cb7b7f3
onecheck <1548767361@qq.com>
2024-05-14 01:53:44 +0800

    add hieratical embodied intelligence

---
+++ dependency/core/lib/algorithms/schedule_agent/hei/drl/Adapter.py
@@ -0,0 +1,20 @@
+
+def Done_adapter(done, t):
+    if t % 100 == 0:
+        return not done
+    else:
+        return done
+
+
+def Action_adapter(a, max_action):
+    """from [-1,1] to [-max,max]"""
+    a[0] *= max_action[0]
+    a[1] *= max_action[1]
+    a[2] *= max_action[2]
+
+    return abs(a)
+
+
+def Action_adapter_reverse(act, max_action):
+    # from [-max,max] to [-1,1]
+    return act / max_action
--- dependency/core/lib/algorithms/schedule_agent/hieratical_embodied_intelligence.py
+++ dependency/core/lib/algorithms/schedule_agent/hieratical_embodied_intelligence.py
@@ -8,8 +8,18 @@ __all__ = ('HieraticalEmbodiedIntelligence',)
 
 @ClassFactory.register(ClassType.SCH_AGENT, alias='hieratical_embodied_intelligence')
 class HieraticalEmbodiedIntelligence(BaseAgent, abc.ABC):
+
     def __init__(self):
         pass
 
-    def __call__(self):
+    def get_schedule_plan(self, info):
+        pass
+
+    def run(self, scheduler):
+        pass
+
+    def update_scenario(self, scenario):
+        pass
+
+    def update_resource(self, resource):
         pass
