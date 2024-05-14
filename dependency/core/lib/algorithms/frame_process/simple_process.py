b66bf454312910ec86b8a08c9501b6a1b08878ce
onecheck <1548767361@qq.com>
2024-05-15 01:32:14 +0800

    add hieratical embodied intelligence

--- dependency/core/lib/algorithms/frame_process/simple_process.py
+++ dependency/core/lib/algorithms/frame_process/simple_process.py
@@ -2,7 +2,7 @@ import abc
 
 from core.lib.common import ClassFactory, ClassType
 from core.lib.common import VideoOps
-from core.lib.common import LOGGER
+
 from .base_process import BaseProcess
 
 __all__ = ('SimpleProcess',)
@@ -17,5 +17,4 @@ class SimpleProcess(BaseProcess, abc.ABC):
         import cv2
 
         resolution = VideoOps.text2resolution(system.meta_data['resolution'])
-        LOGGER.debug(f'[Frame Process] raw: {frame.shape} / new:{resolution}')
         return cv2.resize(frame, resolution)
